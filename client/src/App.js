43659387e0ebe1109c1b4a326e4141d37ae06c93
CSI-WEB <CSI-WEB@campus.local>
2023-05-03 16:40:53 +0200

    init menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import React from 'react'
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
  
-import CreateChapter from "./pages/CreateChapter";
 
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 library.add(faHome, faHeart, faUser, );
 
@@ -19,14 +20,10 @@ function App(){
   return <div className="App">
     <header  className="header">
     <BrowserRouter className="">
-      <Link className ="p-2 bg-neutral-700	" to='/createMangas'>Create a Manga</Link>
       <Link to='/'>Home</Link>
       <Link to='login'><FontAwesomeIcon icon="user"/></Link>
       <Routes>
-        <Route path='/createMangas' element={<CreateMangas/>} />
         <Route path='/' element={<Home/>} />
-        <Route path='/manga/:id' element={<Manga/>}/>
-        <Route path='/createChapter/:id' element={<CreateChapter/>}/>
         <Route path='/login' element={<Login/>}/>
 
       </Routes>
