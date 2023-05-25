5656445ba996198575c7f41834f37dba431e923d
slorochi <94987200+slorochi@users.noreply.github.com>
2023-05-25 23:23:50 +0200

    profile

--- client/src/components/mediatheque/Mediatheque.jsx
+++ client/src/components/mediatheque/Mediatheque.jsx
@@ -5,6 +5,8 @@ import Dropzone from "dropzone";
 import "./Mediatheque.css";
 import { AuthContext } from '../../contexts/AuthContext';
 
+// icons
+import {MdDownload} from 'react-icons/md'
 /* import { SocketContext } from "../utils/socket";
  */
 
@@ -29,14 +31,15 @@ function Mediatheque(
   
 ) {
   const [listOfFiles, setListOfFiles] = useState([]);
-  const [dropMessage, setDropMessage] = useState("no-class");
+  const [selectedFile, setSelectedFile] = useState(null);
+
   /* const socket = useContext(SocketContext); */
   const { userAuthenticated } = useContext(AuthContext);
 
+  const inputRef = useRef(null)
 
   console.log(userAuthenticated)
-  const dropzoneRef = useRef("");
-  const myDropzone = useRef("");
+
 
   useEffect(() => {
     // afin de ne pas avoir l'id de l'utilisateur dans le context, on utilise l'email:
@@ -45,35 +48,25 @@ function Mediatheque(
     axios.get(`http://localhost:3001/files/byUserEmail/${email}`).then((response) => {
       setListOfFiles(response.data);
     });
-    postDropzone();
   }, []);
 
-  // dropzone div
-
-  //actualiser la liste des épisodes lors d'un changement
-  /* socket.on("refreshMediathequeUi", function (data) {
-    console.log(data);
-    setListOfFiles(data);
-    setListOfFilteredFiles(data);
-  });
- */
-
-  /* #TODO: vérifier sur le delete d'un file qu'il n'est pas lié à un post*/
-  /* const deleteFile = (id) => {
-    axios.delete(`${API_URL}/files/${id}`).then((response) => {
-      console.log(response);
-    });
-  }; */
-  const postDropzone = () => {
+  const styleInput= {
+    position: "absolute",
+    marginTop: 3,
+    marginLeft: 3,
+    height: 1,
+    width: 1,
+    zIndex: -5,
+}
+  const handleCallClickInput = (e)=>{inputRef.current.click()
+  }
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    console.log(selectedFile);
+  };
+ /*  const postDropzone = () => {
     if (!myDropzone.current) {
       myDropzone.current = new Dropzone(dropzoneRef.current, dropzoneOptions);
-      // si on ajoute un fichier, display none le message dans la dropbox
-      myDropzone.current.on("addedfile", (file) => {
-        setDropMessage("dz-message");
-      });
-      // myDropzone.current.on("error", (file) => console.log("erreur", file));
-      // si on annule un fichier le message s'affiche a nouveau
-      myDropzone.current.on("removedfile", () => setDropMessage("no-class"));
       myDropzone.current.on("success", (file) => {
         console.log(file);
         console.log("success");
@@ -86,7 +79,6 @@ function Mediatheque(
           mime_type: file.type,
         };
         axios.post(`http://localhost:3001/files/merge-chunk`, json).then((response) => {
-          /* socket.emit("askRefresh"); */
           console.log(response);
         });
       });
@@ -95,33 +87,38 @@ function Mediatheque(
       });
     }
   };
+ */
+  const handleUpload = () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
 
-
+    axios.post('http://localhost:3001/upload', formData)
+      .then((res) => {
+        console.log(res.data);
+        // Traitez la réponse du serveur ici si nécessaire
+      })
+      .catch((err) => {
+        console.error(err);
+        // Gérez les erreurs ici si nécessaire
+      });
+  };
   return (
     <>
-      <div
-        style={{
-          /*   border: "2px solid black", */
-          padding: "18px 28px",
-          borderRadius: 4,
-          boxShadow: " 0px 0px 12px -4px rgb(122,122,122)",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "360px",
-        }}
-      >
-        <h2 style={{ textAlign: "center" }}>Télécharger votre fichier</h2>
+     <input style={styleInput} type="file" ref={inputRef} onClick={()=>{console.log("hello")}} onChange={handleFileChange} />
         <div
-          className="dropzone drop-container"
-          /*    id="drop-form" */
-          ref={dropzoneRef}
-        ></div>
-      </div>
+          onClick={handleCallClickInput}
+          className=" h-[50px] rounded-[50px]"
+        ><div className=" btn-slide2 cursor-pointer" > 
+          <span className="circle2 flex items-center justify-center">
+            <MdDownload size={20}/></span><span className="title2">Upload</span>  
+          <span className="title-hover2">Click here</span>
+        </div>
+      
+        </div>
       <div
         style={{
           boxShadow: " 0px 0px 12px -4px rgb(122,122,122)",
-          marginTop: 22,
+          marginTop: 52,
           width: "100%",
           padding: "18px 10px",
           display: "flex",
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useState,useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
+import {Button, Modal} from 'antd';
 //components
 import Mediatheque from '../components/mediatheque/Mediatheque';
 
@@ -9,21 +9,41 @@ const Profile = () => {
   const { userAuthenticated } = useContext(AuthContext);
   console.log(userAuthenticated);
   const navigate = useNavigate();
-
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   useEffect(()=>{
    if(!userAuthenticated){
       navigate("/login");
     } 
   })
   return (
-    <div className="flex relative top-[-60px] z-10 flex-col w-full items-center">
+    
+    <div className="flex relative top-[60px] z-10 flex-col w-full items-center">
         {/* banner */}
-        <div className="h-[500px] w-full" style={{
+        <div className="h-[500px] w-full flex items-end p-10 justify-center" style={{
     background: `url(${process.env.PUBLIC_URL}/25_04_01.png) no-repeat center  fixed`,
     backgroundSize: "cover",
-  }}></div>
-        <p className="h-[1600px] w-full" style={{}}>Bienvenue sur votre profil</p>
-        <Mediatheque userAuthenticated={userAuthenticated}/>
+  }}>
+    <button className="w-[140px] text-sm h-[35px] border-2 transition ease-in-out duration-200 border-[#f1f1f18c] rounded-[20px] text-[#f1f1f18c] hover:border-white hover:text-white"  onClick={showModal}>
+        Modifier
+      </button>
+      <Modal title="Importer un fichier" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <div className="flex flex-col items-center">
+          <Mediatheque userAuthenticated={userAuthenticated}/>
+        </div>
+      </Modal>
+  </div>
+        <p className=" w-full" style={{}}>Bienvenue sur votre profil</p>
+       
     </div>
   );
 };
